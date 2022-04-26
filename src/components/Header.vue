cd735b38a97af989ef327d12497c4f12c0c6d38e
Stefano Calzoni <stefanoclz@hotmail.it>
2022-04-26 14:58:34 +0200

    creato componente aggiuntivo per riciclare i bottoni laterali e aggiunti in header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,22 +2,19 @@
   <header>
     <Nav />
     <img src="@/assets/data/img/h3-rev-img-6.png" alt="pizza slice" />
-    <div class="btn-left">
-      <h4>PREV</h4>
-    </div>
-    <div class="btn-right">
-      <h4>NEXT</h4>
-    </div>
+    <PrevNextButton />
   </header>
 </template>
 
 <script>
 import Nav from "@/components/Nav.vue";
+import PrevNextButton from "@/components/PrevNextButton.vue";
 
 export default {
   name: "headerComponent",
   components: {
     Nav,
+    PrevNextButton,
   },
 };
 </script>
@@ -35,36 +32,5 @@ header {
 
 img {
   margin: 25px 0px;
-}
-
-h4 {
-  color: #d34523;
-  margin-bottom: 10px;
-}
-
-.btn-right {
-  position: absolute;
-  top: 50%;
-  right: -15px;
-  transform: rotate(-90deg);
-  border-radius: 50px 50px 0px 0px;
-  padding: 15px 10px 0px 10px;
-  border: none;
-  font-size: 10px;
-  width: 60px;
-  background-color: white;
-}
-
-.btn-left {
-  position: absolute;
-  left: -15px;
-  top: 50%;
-  transform: rotate(90deg);
-  border-radius: 50px 50px 0px 0px;
-  padding: 15px 10px 0px 10px;
-  border: none;
-  font-size: 10px;
-  background-color: white;
-  width: 60px;
 }
 </style>
