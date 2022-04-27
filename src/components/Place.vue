b23b90047c064c3e1d7393a2ba50e25a45892b4a
Stefano Calzoni <stefanoclz@hotmail.it>
2022-04-27 14:39:51 +0200

    aggiunti hover come da richiesta esercitazione

--- src/components/Place.vue
+++ src/components/Place.vue
@@ -2,15 +2,35 @@
   <section>
     <div class="card">
       <img src="@/assets/data/img/h3-img-1.jpg" alt="restaurant" />
+      <div class="detail">
+        <div class="circle">
+          <i class="fa-regular fa-eye"></i>
+        </div>
+      </div>
     </div>
     <div class="card">
       <img src="@/assets/data/img/h3-img-2.jpg" alt="Eat Good Pizza" />
+      <div class="detail">
+        <div class="circle">
+          <i class="fa-regular fa-eye"></i>
+        </div>
+      </div>
     </div>
     <div class="card">
       <img src="@/assets/data/img/h3-img-3.jpg" alt="Yum" />
+      <div class="detail">
+        <div class="circle">
+          <i class="fa-regular fa-eye"></i>
+        </div>
+      </div>
     </div>
     <div class="card">
       <img src="@/assets/data/img/h3-img-4.jpg" alt="Bigger pizza" />
+      <div class="detail">
+        <div class="circle">
+          <i class="fa-regular fa-eye"></i>
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -33,6 +53,34 @@ section {
     img {
       width: 100%;
     }
+
+    .detail {
+      width: 100%;
+      height: 100%;
+      background-image: url(@/assets/data/img/background_dotted.jpg);
+      display: none;
+      justify-content: center;
+      align-items: center;
+
+      .circle {
+        padding: 30px;
+        background-color: #d2401e;
+        border-radius: 50%;
+        color: white;
+
+        i {
+          font-size: 25px;
+        }
+      }
+    }
   }
+}
+
+.card:hover img {
+  display: none;
+}
+
+.card:hover .detail {
+  display: flex;
 }
 </style>
